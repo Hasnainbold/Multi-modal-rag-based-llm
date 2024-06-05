511a368112430b26185e2abcc060ebd8d030784a
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-06-05 15:41:13 +0530

    added 2 layer refiner

--- Rag_chain.py
+++ Rag_chain.py
@@ -31,7 +31,6 @@ from typing import Sequence
 import pandas as pd
 from Vector_database import VectorDatabase
 
-
 class RAGEval:
     '''
     WorkFlow:
@@ -41,16 +40,10 @@ class RAGEval:
     4. Call query()
     5. Call raga()
     '''
-    best = 3
+    best = 2
     parse = StrOutputParser()
 
-
-    def __init__(self,file_path,url,vb_key,gpt_key, embed_model, cross_model):
-        # self.vector_db(file_path,"https://chatgpt-db-z0lfxjds.weaviate.network","wVmxx6E57W2zJueEb8S1o3cJjwRiaAJEMkRA", embed_model)
-        # os.environ["OPENAI_API_KEY"] = gpt_key
-        #self.chatgpt = ChatOpenAI(model="gpt-4")
-        #self.chat_model = self.chatgpt
-        #self.parser = self.parse
+    def __init__(self,file_path, vb_list, cross_model): # vb_list = [(vb,splitter)]
         self.cross_model = cross_model
         self.template = """You are an assistant for question-answering tasks.
         Use the following pieces of retrieved context to answer the question.
@@ -61,69 +54,38 @@ class RAGEval:
         Answer:
         """
         self.file_path = file_path
-        self.url = url
-        self.vb_key = vb_key
-        self.embed_model = embed_model
-        self.vector_db(self.file_path, self.url, self.vb_key, self.embed_model)
+        self.vb_list = vb_list
+        self.vector_dbs()
 
     def ground_truths_prep(self,questions): # questions is a file with questions
         self.ground_truths = [[s] for s in self.query(questions)]
         self.vector_db(self.file_path, self.url, self.vb_key, self.embed_model)
 
-    def vector_db(self,file_path, url, api_key, embed_model): # file_path is the file of dataset
-        # Read the content of the file
-        with open(file_path, 'rb') as file:
-            data = file.read()
-
-        # Create a Document object
-        documents = [Document(page_content=data)]
-        # Initialize the text splitter
-        text_splitter = CharacterTextSplitter(chunk_size=1330, chunk_overlap=50)
-        # Split the documents into chunks
-        chunks = text_splitter.split_documents(documents)
-
-        WEAVIATE_CLUSTER=url
-        WEAVIATE_API_KEY=api_key
-        client=weaviate.Client(embedded_options=EmbeddedOptions())
-
-        # Initialize the custom embedding model
-        embedding_model = embed_model
-
-        # Assuming you have a Weaviate client and documents prepared as `client` and `chunks`
-        # Create the Weaviate vector store
-        vectorstore = Weaviate.from_documents(
-            client=client,
-            documents=chunks,
-            embedding=embedding_model,
-            by_text=False
-        )
-        self.retriever=vectorstore.as_retriever()
+    def vector_dbs(self):
+      with open(self.file_path, 'r') as file:
+        data = file.read()
+      for vb,sp in self.vb_list:
+        vb.upsert(data,sp)
 
     def model_prep(self,model,parser_choice=parse): # model_link is the link to the model
         self.chat_model = model
         self.parser = parser_choice
 
-    # def rag_chain(self,model):
-    #     # Define prompt template
-    #     self.prompt = ChatPromptTemplate.from_template(self.template)
-    #     self.ragchain=(
-    #               {
-    #                   "context":self.cross_model.rank(query=RunnablePassthrough(), documents=self.retriever,return_documents=True),
-    #                   "question":RunnablePassthrough()
-    #               }
-    #               | self.prompt
-    #               | model
-    #     )
-
     def query(self,question):
         self.questions = question
+        prior_context = [vb.query(question) for vb,_ in self.vb_list]
+        cont = []
+        for i in prior_context:
+          context = ""
+          for j in i: # list to str
+            context += j
+          cont.append(context)
 
-        prior_context = [docs.page_content for docs in self.retriever.get_relevant_documents(self.questions)]
         c = self.cross_model.rank(
               query=question,
-              documents=prior_context,
+              documents=cont,
               return_documents=True
-            )[:len(prior_context)-2]
+            )[:len(prior_context)-self.best]
         self.context = [i['text'] for i in c]
 
         self.answers = self.parser.invoke(
